a5f924a511029d6f2a28685e473a680a8b2bf144
Cashman1396 <Chrisashman96@gmail.com>
2021-01-24 01:20:23 -0500

    finished element code

--- src/components/element.js
+++ src/components/element.js
@@ -1,6 +1,4 @@
-const { Component } = require("react")
-
-import React { Component } from 'react';
+import React, { Component } from 'react';
 import { elementals } from './data';
 
 
@@ -20,4 +18,21 @@ class Element extends Component {
     onMouseLeave = (event) => {
         this.setState({ hover: false })
     }
+
+    render() {
+        let { num } = this.props;
+        let element = elements[num]
+        return (
+            <div
+            title={element.name} onMouseEnter={this.onMouseEnter}
+            onMouseLeave={this.onMouseLeave} onClick={this.openInfo} 
+            className = {`element element-${num} ${element.category} ${this.state.hover ? 'active' : ''}`}>
+                <div className="number">{element.number}</div>
+                <div className="symbol">{element.symbol}</div>
+                <div className="elementname">{element.name}</div>
+            </div>
+        )
+    }
 }
+
+export default Element
