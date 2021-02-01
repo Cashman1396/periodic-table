1e963784251591a100751479585cd9cc38771e3f
Cashman1396 <Chrisashman96@gmail.com>
2021-02-01 10:35:24 -0500

    added antimony

--- src/components/data.js
+++ src/components/data.js
@@ -1216,6 +1216,30 @@ module.exports = {
             ypos: 5,
             shells: [2, 8, 18, 18, 4]
           },
+          {
+            name: 'Antimony',
+            appearance: 'silvery lustrous gray',
+            atomic_mass: 121.7601,
+            boil: 1908,
+            category: 'metalloid',
+            color: null,
+            density: 6.697,
+            discovered_by: 'unknown, before 3000 BC',
+            melt: 903.78,
+            molar_heat: 25.23,
+            named_by: null,
+            number: 51,
+            period: 5,
+            phase: 'Solid',
+            source: 'https://en.wikipedia.org/wiki/Antimony',
+            spectral_img: null,
+            summary:
+              'Antimony is a chemical element with symbol Sb (from Latin:stibium) and atomic number 51. A lustrous gray metalloid, it is found in nature mainly as the sulfide mineral stibnite (Sb2S3). Antimony compounds have been known since ancient times and were used for cosmetics; metallic antimony was also known, but it was erroneously identified as lead upon its discovery.',
+            symbol: 'Sb',
+            xpos: 15,
+            ypos: 5,
+            shells: [2, 8, 18, 18, 5]
+          },
 
 
     ]
